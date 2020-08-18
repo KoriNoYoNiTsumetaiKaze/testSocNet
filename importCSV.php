87bb438c76ab3573360e5d7568e9e5e109051238
KoriNoYoNiTsumetaiKaze <darkanddeep@yandex.ru>
2020-08-19 00:23:13 +0700

    progress commit.

--- importCSV.php
+++ importCSV.php
@@ -1,9 +1,19 @@
 <?php
-if (($fp = fopen("import.csv", "r")) !== FALSE) {
+$file = "import.csv";
+$countLines = count(file($file));
+//print_r($countLines);
+$step = $countLines/100;
+$progress = 0;
+if (($fp = fopen($file, "r")) !== FALSE) {
 	while (($data = fgetcsv($fp, 0, ";")) !== FALSE) {
+        print_r(chr(8).chr(8).chr(8).chr(8));
 		$list[] = $data;
+        $progress = floor($progress+$step);
+        if ($progress>100) $progress = 100;
+        print_r((string)$progress."%");
+        //sleep(1);
 	}
 	fclose($fp);
-	print_r($list);
+	//print_r($list);
 }
 ?>
